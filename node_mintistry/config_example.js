84059312ed9a23d9d182a673387d22790413ee0a
lukasmarshall <lukasmarshall@gmail.com>
2015-10-19 15:10:43 -0700

    Added config_example.js file - when implementing, change values and rename to config.js

---
+++ node_mintistry/config_example.js
@@ -0,0 +1,8 @@
+module.exports.params={
+	ip_address:"127.0.0.1",
+	port: 1337,
+	geth_url: "http://127.0.0.1:8545",
+	https: false,
+	ssl_key_location:'keys/key.pem',
+	ssle_cert_location:'keys/cert.pem',
+}
